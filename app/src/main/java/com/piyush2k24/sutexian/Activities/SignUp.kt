fcb3a7a49438ccfbe937fcccf752cfe25d843a2c
Piyush Makwana <piyush2k24@gmail.com>
2023-04-20 20:57:35 +0530

    Ui Completed

---
+++ app/src/main/java/com/piyush2k24/sutexian/Activities/SignUp.kt
@@ -0,0 +1,12 @@
+package com.piyush2k24.sutexian.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.piyush2k24.sutexian.R
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_up)
+    }
+}
---
+++ app/src/main/java/com/piyush2k24/sutexian/Startup/GetStarted.kt
@@ -0,0 +1,36 @@
+package com.piyush2k24.sutexian.Startup
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.piyush2k24.sutexian.Activities.SignIn
+import com.piyush2k24.sutexian.Activities.SignUp
+import com.piyush2k24.sutexian.R
+import com.piyush2k24.sutexian.databinding.GetStartedBinding
+
+class GetStarted : AppCompatActivity() {
+    private lateinit var binding: GetStartedBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = GetStartedBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        callers();
+    }
+
+    private fun callers(){
+        binding.SignUp.setOnClickListener {
+            showToast("Sign Up 🤥")
+            startActivity(Intent(this@GetStarted,SignUp::class.java))
+        }
+        binding.SignIn.setOnClickListener{
+            showToast("Sign In 😒")
+            startActivity(Intent(applicationContext,SignIn::class.java))
+        }
+    }
+
+    private fun showToast( str: String){
+        Toast.makeText(this@GetStarted,str,Toast.LENGTH_SHORT).show()
+    }
+}
