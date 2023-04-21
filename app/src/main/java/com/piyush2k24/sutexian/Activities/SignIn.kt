132c5f38f097b0bf95d81ac39a6ffee2ea2fd6ec
Piyush Makwana <piyush2k24@gmail.com>
2023-04-21 09:37:03 +0530

    All Done SignIn/SignUp

--- app/src/main/java/com/piyush2k24/sutexian/Activities/SignIn.kt
+++ app/src/main/java/com/piyush2k24/sutexian/Activities/SignIn.kt
@@ -1,12 +1,52 @@
 package com.piyush2k24.sutexian.Activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.piyush2k24.sutexian.R
+import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.piyush2k24.sutexian.MainActivity
+import com.piyush2k24.sutexian.databinding.SignInBinding
 
 class SignIn : AppCompatActivity() {
+    private lateinit var binding: SignInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.sign_in)
+        binding=SignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        caller();
+    }
+
+    private fun caller(){
+        binding.SignIn.setOnClickListener{
+            if(isValidate()){
+                startActivity(Intent(this@SignIn,MainActivity::class.java))
+                showToast("Successfully SignIn !")
+            }
+        }
+        binding.CreateAnNewAccount.setOnClickListener{
+            startActivity(Intent(applicationContext,SignUp::class.java))
+        }
+
+    }
+    private fun isValidate(): Boolean {
+        if(binding.EmailId.text.toString().isEmpty()){
+            showToast("Please Enter Your Email");
+            return false
+        }else if (!Patterns.EMAIL_ADDRESS.matcher(binding.EmailId.text.toString()).matches()){
+            showToast("Please Enter Valid Email")
+            return false
+        }else if (binding.Password.text.toString().isEmpty()){
+            showToast("Password Can't be Empty")
+            return false
+        }else if (binding.Password.text.toString().length<6){
+            showToast("Password length must be 6 char")
+            return false
+        }
+        return true
+    }
+
+    private fun showToast(str: String){
+        Toast.makeText(applicationContext,str,Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/piyush2k24/sutexian/Activities/SignUp.kt
+++ app/src/main/java/com/piyush2k24/sutexian/Activities/SignUp.kt
@@ -1,12 +1,107 @@
 package com.piyush2k24.sutexian.Activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener
+import com.piyush2k24.sutexian.MainActivity
 import com.piyush2k24.sutexian.R
+import com.piyush2k24.sutexian.databinding.SignUpBinding
 
 class SignUp : AppCompatActivity() {
+    private lateinit var binding: SignUpBinding
+    private lateinit var materialDatePicker: MaterialDatePicker<Long>
+    private lateinit var arrayAdapter:ArrayAdapter<String>
+    var CountryNo= arrayOf(
+        "+91","+01","+41","+49","+45","+46","+47","+55",
+        "+81","+92","+93","+94"
+        );
+    var Colleges= arrayOf(
+        "Sutex Bank College","J.J Shah College","Pro, V.B Shah College","Akhand Anand College",
+        "Mahavir College","SVNIT","S.V Patel College","SDJ College","Govt. Medical College"
+    );
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.sign_up)
+        binding= SignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        caller();
+    }
+    private fun caller(){
+        binding.SignUp.setOnClickListener{
+            if(isValidate()){
+                startActivity(Intent(this@SignUp,MainActivity::class.java))
+                showToast("Successfully SignUp")
+            }
+        }
+        binding.AlreadyHaveAnAccount.setOnClickListener{
+            startActivity(Intent(applicationContext,SignIn::class.java))
+        }
+        BirthDatePicker()
+        MaterialSpinner()
+    }
+
+    private fun isValidate(): Boolean{
+        if(binding.Fname.text.toString().isEmpty()){
+            showToast("Enter Your FirstName")
+            return false
+        }else if (binding.Lname.text.toString().isEmpty()){
+            showToast("Enter Your LastName")
+            return false
+        }else if (binding.Birthdate.text.toString().isEmpty()){
+            showToast("Please Choose Your Birthdate ")
+            return false
+        }else if(!binding.Male.isChecked && !binding.Female.isChecked && !binding.Other.isChecked){
+            showToast("Please Choose Your Gender")
+            return false
+        }else if(binding.EmailId.text.toString().isEmpty()){
+            showToast("Please Enter Your Email");
+            return false
+        }else if (!Patterns.EMAIL_ADDRESS.matcher(binding.EmailId.text.toString()).matches()) {
+            showToast("Please Enter Valid Email")
+            return false
+        }else if (binding.CountryNo.text.toString().isEmpty()){
+            showToast("Choose Your Country Code")
+            return false
+        }else if (binding.PhoneNo.text.toString().isEmpty()){
+            showToast("Enter Your PhoneNo")
+            return false
+        }else if(binding.PhoneNo.text.toString().length<10 || binding.PhoneNo.text.toString().length>10){
+            showToast("Phone No Should be 10 digit")
+            return false
+        }else if (binding.AmroliColleges.text.toString().isEmpty()){
+            showToast("Please Choose The college")
+            return false
+        }
+        return true
+    }
+    private fun BirthDatePicker(){
+        materialDatePicker= MaterialDatePicker.Builder.datePicker()
+            .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+            .setTitleText("Choose Birthdate")
+            .build()
+
+        binding.btnBirthdate.setOnClickListener{
+            materialDatePicker.show(supportFragmentManager,"Kotlin Material datePicker")
+            materialDatePicker.addOnPositiveButtonClickListener(
+                MaterialPickerOnPositiveButtonClickListener {
+                    binding.Birthdate.setText(materialDatePicker.headerText)
+                }
+            )
+        }
+    }
+    private fun MaterialSpinner(){
+        arrayAdapter= ArrayAdapter<String>(applicationContext, R.layout.cust_spinner,CountryNo)
+        binding.CountryNo.setAdapter(arrayAdapter)
+
+        arrayAdapter= ArrayAdapter<String>(this@SignUp,R.layout.cust_spinner,Colleges)
+        binding.AmroliColleges.setAdapter(arrayAdapter)
+    }
+
+    private fun showToast(str : String){
+        Toast.makeText(this@SignUp,str,Toast.LENGTH_SHORT).show()
     }
 }
