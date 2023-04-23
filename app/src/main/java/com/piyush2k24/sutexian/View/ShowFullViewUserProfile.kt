553c032c1ad5334859ad2a7a969b8d1c124767f0
Piyush Makwana <piyush2k24@gmail.com>
2023-04-23 13:08:36 +0530

    added 2 Button (BackButton || Firebase SignOut)

--- app/src/main/java/com/piyush2k24/sutexian/View/ShowFullViewUserProfile.kt
+++ app/src/main/java/com/piyush2k24/sutexian/View/ShowFullViewUserProfile.kt
@@ -2,16 +2,21 @@ package com.piyush2k24.sutexian.View
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.piyush2k24.sutexian.Activities.SignIn
 import com.piyush2k24.sutexian.R
 import com.piyush2k24.sutexian.databinding.ShowFullViewUserProfileBinding
 
 class ShowFullViewUserProfile : AppCompatActivity() {
     private lateinit var binding: ShowFullViewUserProfileBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ShowFullViewUserProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        firebaseAuth=FirebaseAuth.getInstance()
         Caller()
     }
     private fun Caller(){
@@ -21,8 +26,17 @@ class ShowFullViewUserProfile : AppCompatActivity() {
         binding.UserPhone.setText(intent.getStringExtra("UserPhone"))
         binding.UserDesignation.setText(intent.getStringExtra("UserDesignation"))
 
-        binding.Back.setOnClickListener {
+        binding.BackForward.setOnClickListener{
             startActivity(Intent(this@ShowFullViewUserProfile,DemoUsersList::class.java))
         }
+
+        binding.LogOut.setOnClickListener{
+            firebaseAuth.signOut()
+            showToast("SignOut Successfully")
+            startActivity(Intent(this@ShowFullViewUserProfile,SignIn::class.java))
+        }
+    }
+    private fun showToast(str: String) {
+        Toast.makeText(applicationContext,str,Toast.LENGTH_SHORT).show()
     }
 }
